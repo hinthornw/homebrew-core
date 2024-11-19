a782152891b4c73585b6d542f1ed133d2cde7673
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-11-19 01:13:28 -0800

    Update langgraph-cli: Tue Nov 19 09:13:28 UTC 2024

--- Formula/l/langgraph-cli.rb
+++ Formula/l/langgraph-cli.rb
@@ -3,25 +3,23 @@ class LanggraphCli < Formula
 
   desc "Command-line interface for deploying apps to the LangGraph platform"
   homepage "https://www.github.com/langchain-ai/langgraph"
-  url "https://files.pythonhosted.org/packages/f1/5c/e8fc086a2c4e7021ec87088165f02f5c5e4f7441f864b0809ed49ba50d79/langgraph_cli-0.1.54.tar.gz"
-  sha256 "2714a62616ae9d9d7363d8a597f18c85aa38b8ed27bc85d6106c98da67e796eb"
-  license "MIT"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "b95b5ca15b8682b4822a788252d184ba1e42bbb0a94f9fc8b0bf0fbe6b4d0bb7"
-  end
+  url "https://files.pythonhosted.org/packages/4c/f0/d284afb29731ccf2668e52a3ce255e445e492b5e3dc50c7fe1102ad5731d/langgraph_cli-0.1.55.tar.gz"
+  sha256 "791007ec3f4ed50b2f9acac252cc4eb6f6d8a97f5dc90f4d5cc2bd3ebf048bf5"
+  license \"MIT\"
 
   depends_on "python@3.13"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
     sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  license \"MIT\"
   end
 
   def install
     virtualenv_install_with_resources
   end
 
+  
   test do
     (testpath/"graph.py").write <<~PYTHON
       from langgraph.graph import StateGraph
@@ -46,4 +44,5 @@ class LanggraphCli < Formula
     dockerfile_content = File.read("DOCKERFILE")
     assert_match "FROM", dockerfile_content, "DOCKERFILE should contain 'FROM'"
   end
+
 end
